017bff57436b1c0b5f387eac570819c0ba5c19e4
peacefrog <tawsif93@gmail.com>
2017-03-16 02:23:53 +0600

    Firebase and  @angular/Material added
    editor config tab size changed and files formatted to tab 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,38 +2,39 @@ import { Component } from '@angular/core';
 import {FormBuilder, Validators, FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector   : 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls  : ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'app works! Yeah';
-
-  public loginForm = this.fb.group({
-    email: ["", this.containsMagicWord],
-    password: ["", Validators.required]
-  });
-
-  private containsMagicWord(c: FormControl) {
-  if(c.value.indexOf('magic') >= 0) {
-    console.log('not valid');
-    return {
-      noMagic: true
-    }
-  }
-
-  // Null means valid, believe it or not
-  console.log('valid');
-  return null
-}
-
-  constructor(public fb: FormBuilder) {}
-
-  doLogin(event) {
-    console.log(event);
-    console.log(this.loginForm.value);
-  }
+	title = 'app works! Yeah';
+	
+	public loginForm = this.fb.group({
+		email   : ["", this.containsMagicWord],
+		password: ["", Validators.required]
+	});
+	
+	private containsMagicWord(c: FormControl) {
+		if (c.value.indexOf('magic') >= 0) {
+			console.log('not valid');
+			return {
+				noMagic: true
+			}
+		}
+		
+		// Null means valid, believe it or not
+		console.log('valid');
+		return null
+	}
+	
+	constructor(public fb: FormBuilder) {
+	}
+	
+	doLogin(event) {
+		console.log(event);
+		console.log(this.loginForm.value);
+	}
 }
 
 
--- src/app/app.formvalidations.ts
+++ src/app/app.formvalidations.ts
@@ -62,23 +62,23 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class FormValidationComponent {
 	complexForm: FormGroup;
-
+	
 	constructor(fb: FormBuilder) {
 		this.complexForm = fb.group({
 			/*To add a validator, we must first convert the string value into an array.
 			 The first item in the array is the default value if any,
 			 then the next item in the array is the validator.
-			 Here we are adding a required validator meaning that 
+			 Here we are adding a required validator meaning that
 			 the firstName attribute must have a value in it.*/
-
+			
 			'firstName': [null, Validators.required],
 			/*
 			 We can use more than one validator per field.
-			 If we want to use more than one validator 
+			 If we want to use more than one validator
 			 we have to wrap our array of validators with a Validators.compose function.
 			 Here we are using a required,
 			 minimum length and maximum length validator.*/
-
+			
 			'lastName': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
 			'gender': [null, Validators.required],
 			'hiking': false,
@@ -86,7 +86,7 @@ export class FormValidationComponent {
 			'swimming': false
 		});
 	}
-
+	
 	submitForm(value: any) {
 		console.log(value);
 	}
