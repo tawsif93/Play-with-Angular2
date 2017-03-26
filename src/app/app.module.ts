21ceddec24f65d0716b3963dbc6391cc14eaebc9
peacefrog <tawsif93@gmail.com>
2017-03-27 03:04:37 +0600

    format indent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {RouterModule, Routes} from '@angular/router'
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {MaterialModule} from '@angular/material';
-import {AngularFireModule} from 'angularfire2';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MaterialModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 
