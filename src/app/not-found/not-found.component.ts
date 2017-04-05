10e94c57f2f791f5c429c43eb67e41616151c6dc
peacefrog <tawsif93@gmail.com>
2017-04-05 21:49:02 +0600

    Material loader added

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 	template: `
 		<div class="jumbotron text-center">
 			<h1>404 Not Found</h1>
-			<p>You may be lost. Follow the breadcrumbs back <a routerLink="/">home</a>.</p>
+			<p>You may  be lost. Follow the breadcrumbs back <a routerLink="/">home</a>.</p>
 		</div>
 	`
 })
